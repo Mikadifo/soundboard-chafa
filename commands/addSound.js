f3a062edc171732863812104d1baf05b1038c945
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-08-11 18:45:00 -0400

    mod: auto create id

--- commands/addSound.js
+++ commands/addSound.js
@@ -1,8 +1,6 @@
 import { SlashCommandBuilder } from "discord.js";
 import { addSound } from "../utils/sounds.js";
 
-//TODO: auto generate ID
-
 const addCommand = {
   data: new SlashCommandBuilder()
     .setName("add")
@@ -15,19 +13,13 @@ const addCommand = {
     )
     .addStringOption((option) =>
       option
-        .setName("id")
-        .setDescription("A unique id for the sound")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
         .setName("url")
         .setDescription("The youtube URL for the sound")
         .setRequired(true)
     ),
   async execute(interaction) {
     const name = interaction.options.getString("name");
-    const id = interaction.options.getString("id");
+    const id = name.toLowerCase().replaceAll(/\s/g, "_");
     const url = interaction.options.getString("url");
 
     addSound({ id, name, url });
