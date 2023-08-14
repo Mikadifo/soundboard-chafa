36ac001be26827ebf63689a7099a0306778be31d
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-08-14 15:29:48 -0400

    add: emoji support for buttons

--- commands/addSound.js
+++ commands/addSound.js
@@ -16,19 +16,28 @@ const addCommand = {
         .setName("url")
         .setDescription("The youtube URL for the sound")
         .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("emoji")
+        .setDescription("A emoji for the sound (use :emoji:)")
+        .setRequired(true)
     ),
   async execute(interaction) {
     const name = interaction.options.getString("name");
     const id = name.toLowerCase().replaceAll(/\s/g, "_");
 
-    const url = interaction.options.getString("url");
     if (soundIdExists(id)) {
       return await interaction.reply(
         `There is a sound with the name \`${name}\` already. Sound not added!`
       );
     }
 
-    addSound({ id, name, url });
+    const emoji = interaction.options.getString("emoji");
+    const url = interaction.options.getString("url");
+    console.log(emoji);
+
+    addSound({ id, name, emoji, url });
     await interaction.reply(`${name} added! Don't forget to restart the bot.`);
   },
 };
--- commands/board.js
+++ commands/board.js
@@ -5,20 +5,25 @@ import {
   SlashCommandBuilder,
 } from "discord.js";
 import { getSounds } from "../utils/sounds.js";
+
 const boardCommand = {
   data: new SlashCommandBuilder()
     .setName("board")
     .setDescription("Show a soundboard with all your sounds."),
   async execute(interaction) {
-    const sounds = getSounds().map(({ id, name }) => ({ id, name }));
+    const sounds = getSounds().map(({ id, name, emoji }) => ({
+      id,
+      name,
+      emoji,
+    }));
     const row = new ActionRowBuilder();
 
     sounds.forEach((sound) => {
       const button = new ButtonBuilder()
         .setCustomId(sound.id)
         .setLabel(sound.name)
-        .setStyle(ButtonStyle.Primary);
-
+        .setStyle(ButtonStyle.Primary)
+        .setEmoji({ name: `${sound.emoji}` });
       row.addComponents(button);
     });
 
